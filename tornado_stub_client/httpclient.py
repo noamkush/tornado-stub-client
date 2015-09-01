1428de22db7e4e8d86017640bb689763936a3385
Noam <noamkush@gmail.com>
2015-09-01 18:20:23 +0300

    Added support for coroutine usage (yield fetch()).

--- tornado_stub_client/httpclient.py
+++ tornado_stub_client/httpclient.py
@@ -1,9 +1,11 @@
+from tornado.concurrent import return_future
 from tornado.httpclient import HTTPRequest, HTTPResponse
 
 from .collection import RequestCollection
 
 class AsyncHTTPStubClient(object):
 
+    @return_future
     def fetch(self, request, callback=None, **kwargs):
         if not isinstance(request, HTTPRequest):
             request = HTTPRequest(url=request, **kwargs)
